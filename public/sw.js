0b8a8361e22c86598332346253091b11f5925d72
P47K0 <pkoorevaar@hotmail.com>
2026-05-21 02:30:58 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'pricecalc-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/calculator',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
